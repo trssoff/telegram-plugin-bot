a066ecb9e8483b44c02513c1796f8e32fe3cf7e3
Michael Tarassov <michael@tarassov.me>
2020-11-04 21:22:02 +0300

    Update Telegram API to 13.0.1, fixed context bug

--- plugins/Hello.py
+++ plugins/Hello.py
@@ -1,21 +1,21 @@
-class Health:
+class Hello:
     def __init__(self):
         self.config = []
         self.commands = {
-            'health': self.health
+            'hello': self.hello
         }
         self.default_config = {
             'test_key': "plugin works fine here!",
             'times_waved': 0
         }
-        self.description = "I'm a health plugin."
-        self.help_text = "Write /health to receive a health information about me!"
+        self.description = "I'm a hello plugin."
+        self.help_text = "Write /hello to receive a health information about me!"
 
-    def health(self, update, context):
+    def hello(self, update, context):
         update.message.reply_text(f'Hi {update.message.from_user.username}! I\'m alive!')
         self.config['times_waved'] += 1
 
     def on_text(self, update, context):
-        if 'health' in update.message.text:
+        if 'hello' in update.message.text:
             update.message.reply_text(f"Hi! Echo: {update.message.text}")
             self.config['times_waved'] += 1
